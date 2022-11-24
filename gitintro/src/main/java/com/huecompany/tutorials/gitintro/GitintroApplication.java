946f1b89edfceb2f8b5a77da5bbd3672b1f9b224
MinhHue <minhhue14072002@gmail.com>
2022-11-24 07:26:22 +0700

    add files generated from Spring Initial

---
+++ gitintro/src/main/java/com/huecompany/tutorials/gitintro/GitintroApplication.java
@@ -0,0 +1,13 @@
+package com.huecompany.tutorials.gitintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitintroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitintroApplication.class, args);
+	}
+
+}
---
+++ gitintro/src/test/java/com/huecompany/tutorials/gitintro/GitintroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huecompany.tutorials.gitintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitintroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
